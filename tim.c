acd4bf27c09308712dda0c3a7ffc15da3263f13c
logeshr63 <logeshr63@gmail.com>
2016-09-19 10:54:37 +0530

    Update and rename README.md to tim.c

---
+++ tim.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, i,;
+    scanf("%d %d", &a, &b);
+   if(a<b)
+   {
+    printf("%d is min",a);
+   }
+    else
+    {
+    printf("%d is min",b);
+}
+    return 0;
